43296e1ff8cbe22d4c1ebd77c3da58a253ed3db0
Ilya <IGandybin@beeline.ru>
2025-06-05 11:44:53 +0300

    Добавлены структуры и методы для обработки данных о действиях пользователя, тренировках и личной информации. Реализованы функции парсинга и получения информации о действиях и тренировках, а также расчета сожженных калорий и дистанции. Обновлены интерфейсы и добавлены проверки на корректность входных данных.

--- internal/spentenergy/spentenergy.go
+++ internal/spentenergy/spentenergy.go
@@ -1,6 +1,7 @@
 package spentenergy
 
 import (
+	"errors"
 	"time"
 )
 
@@ -12,18 +13,69 @@ const (
 	walkingCaloriesCoefficient = 0.5  // коэффициент для расчета калорий при ходьбе.
 )
 
+// WalkingSpentCalories вычисляет количество калорий, потраченных при ходьбе.
+// Принимает количество шагов, вес и рост пользователя, а также продолжительность ходьбы.
+// Возвращает количество потраченных калорий и ошибку, если входные параметры некорректны.
 func WalkingSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
-	// TODO: реализовать функцию
+	if duration <= 0 {
+		return 0, errors.New("duration is less than 0")
+	}
+	if steps <= 0 {
+		return 0, errors.New("steps is less than 0")
+	}
+	if weight <= 0 {
+		return 0, errors.New("weight is less than 0")
+	}
+	if height <= 0 {
+		return 0, errors.New("height is less than 0")
+	}
+	meanSpeed := MeanSpeed(steps, height, duration)
+	callories := ((weight * meanSpeed * duration.Minutes()) / minInH) * walkingCaloriesCoefficient
+	return callories, nil
 }
 
+// RunningSpentCalories вычисляет количество калорий, потраченных при беге.
+// Принимает количество шагов, вес и рост пользователя, а также продолжительность бега.
+// Возвращает количество потраченных калорий и ошибку, если входные параметры некорректны.
 func RunningSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
-	// TODO: реализовать функцию
+	if duration <= 0 {
+		return 0, errors.New("duration is less than 0")
+	}
+	if steps <= 0 {
+		return 0, errors.New("steps is less than 0")
+	}
+	if weight <= 0 {
+		return 0, errors.New("weight is less than 0")
+	}
+	if height <= 0 {
+		return 0, errors.New("height is less than 0")
+	}
+
+	meanSpeed := MeanSpeed(steps, height, duration)
+	callories := (weight * meanSpeed * duration.Minutes()) / minInH
+	return callories, nil
 }
 
+// MeanSpeed вычисляет среднюю скорость движения на основе количества шагов,
+// роста пользователя и продолжительности активности.
+// Возвращает скорость в километрах в час.
+// Если продолжительность активности меньше или равна 0, возвращает 0.
 func MeanSpeed(steps int, height float64, duration time.Duration) float64 {
-	// TODO: реализовать функцию
+	if duration <= 0 {
+		return 0
+	}
+
+	distance := Distance(steps, height)
+	speed := distance / duration.Hours()
+	return speed
 }
 
+// Distance вычисляет расстояние, пройденное пользователем на основе количества шагов и роста.
+// Принимает количество шагов и рост пользователя, возвращает расстояние в километрах.
 func Distance(steps int, height float64) float64 {
-	// TODO: реализовать функцию
+	stepLength := height * stepLengthCoefficient
+	distance := stepLength * float64(steps)
+	distanceKm := distance / mInKm
+
+	return distanceKm
 }
